ea4eb175e14fef5603fedf7742a1c18f31282be8
KavyDave18 <kavydave4922@gmail.com>
2026-06-12 22:31:47 +0530

    add router

---
+++ backend/services/search_router_service.py
@@ -0,0 +1,30 @@
+from backend.services.reranked_search_service import reranked_search_service
+from backend.services.confidence_service import confidence_service
+from backend.services.web_search_service import web_seach_service
+
+class SearchRouterService:
+
+    def search(self,query):
+
+        video_result = (reranked_search_service.search(query=query))
+
+        analysis = (confidence_service.analyze(video_result))
+
+        if analysis["is_confident"]:
+            return {
+                "query": query,
+                "source": "video",
+                "confidence": analysis,
+                "results": video_result
+            }
+
+        web_results=(web_seach_service.search(query=query))
+
+        return {
+            "query": query,
+            "source": "web",
+            "confidence": analysis,
+            "results": web_results
+        }
+
+search_router_service = (SearchRouterService())
